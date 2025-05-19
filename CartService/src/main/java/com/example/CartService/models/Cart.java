1b9ed1b5250c3b38a7ee0544767fb2f84a7230ad
Ritesh Bhardwaj <riteshbhardwaj00001@gmail.com>
2025-05-19 16:04:48 +0530

    Making relation bi-directional between Cart and CartEntry

--- CartService/src/main/java/com/example/CartService/models/Cart.java
+++ CartService/src/main/java/com/example/CartService/models/Cart.java
@@ -1,14 +1,13 @@
 package com.example.CartService.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Entity
 @Table(name = "carts")
@@ -24,5 +23,7 @@ public class Cart extends BaseEntity{
     @Column(precision = 10, scale = 2)
     private BigDecimal cartTotal;
 
+    @OneToMany(mappedBy = "cart" , cascade = CascadeType.ALL)
+    private List<CartEntry> cartEntryList;
 
 }
